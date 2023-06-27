ed0b27ed171f2cf709ab3d26e036aeeec4e85690
Tanguy EDELIN <tedelin@paul-f5Br5s2.clusters.42paris.fr>
2023-06-27 11:59:17 +0200

    fix main ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tedelin <tedelin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/25 21:59:35 by tedelin           #+#    #+#             */
-/*   Updated: 2023/06/26 11:20:50 by tedelin          ###   ########.fr       */
+/*   Updated: 2023/06/27 11:58:18 by tedelin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ int main()
     int tab[] = {1, 2, 3, 4, 5};
     float tab2[] = {1.1, 2.2, 3.3, 4.4, 5.5};
     std::string tab3[] = {"Hello", "World", "!"};
-    iter(tab, 5, my_function);
+    iter(tab, 5, print);
     std::cout << "-----------------" << std::endl;
-    iter(tab2, 5, my_function);
+    iter(tab2, 5, print);
     std::cout << "-----------------" << std::endl;
-    iter(tab3, 3, my_function);
+    iter(tab3, 3, print);
     return 0;   
 }
